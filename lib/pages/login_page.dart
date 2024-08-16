f63bb3d07ab945acaf773d166ca06e7f4f421db0
mahabubulhasibshawon <mahabubulhasibshawon@gmail.com>
2024-08-16 22:40:20 +0600

    login page ui

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../routes/routes.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Login'),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            Text('Hi...Welcome back!',
+            style: Theme.of(context).textTheme.bodyLarge,
+            ),
+            const SizedBox(height: 20,),
+            TextFormField(
+              decoration: const InputDecoration(
+                label: Text('Username'),
+              ),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                label: Text('Password'),
+              ),
+            ),
+            const SizedBox(height: 20,),
+            Row(
+              children: [
+                const Text("Don't have an account?"),
+                TextButton(onPressed: ()=> context.go(Routes.register), child: const Text('Register')),
+              ],
+            ),
+            const SizedBox(height: 20,),
+            ElevatedButton(onPressed: (){}, child: const Text('Login')),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/pages/page_holder.dart
+++ lib/pages/page_holder.dart
@@ -6,6 +6,6 @@ class PageHolder extends StatelessWidget {
   final Widget child;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold();
   }
 }
